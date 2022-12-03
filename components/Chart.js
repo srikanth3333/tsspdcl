f43aaebe1569c47fd5efeb61f568f15a1ef9c4d2
srikanth3333 <srikanth939196@gmail.com>
2022-12-03 19:51:30 +0530

    graphs added

---
+++ components/Chart.js
@@ -0,0 +1,58 @@
+import {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
+import Head from 'next/head';
+
+const Chart = ({id,name,filterId}) => {
+    
+    let user = useSelector((state) => state.users)
+    const [eroId, setEroId] = useState(user?.logData?.eroList?.[0])
+
+    const sdk = new ChartsEmbedSDK({
+        baseUrl: "https://charts.mongodb.com/charts-project-0-sysep",
+    });
+    
+    const chart = sdk.createChart({
+      chartId: id,
+      height:'400px',
+      width:'100%',
+    });
+    
+
+    useEffect(() => {
+        const filterSelect =  document.querySelector(`.ids`)
+        filterSelect.addEventListener('change', async (e) => {
+            if(filterSelect.value == "") {
+              chart.setFilter({})
+            }else {
+              chart.setFilter({ "spoterocd": filterSelect.value })
+            }
+        })
+        chart.render(document.querySelector(`.${name}`))
+        return () => {}
+    }, [])
+
+    return (
+        <>
+            <div>
+                {
+                    !filterId
+                    ? null :
+                    <>
+                        <input type="text" className="test d-none " />
+                        <label htmlFor="">Ero List</label>
+                        <select name="" onChange={(e) => setEroId(e.target.value)} id="" className={`ids form-select mb-3`}>
+                            <option value={""}>{"No Select"}</option>
+                            {user?.logData?.eroList?.map((item,i) => (
+                                <option value={item} key={i}>{item}</option>
+                            ))}
+                        </select>
+                    </>
+                }
+            </div>  
+            <div className={`${name}`}></div>
+        </>
+    );
+}
+
+export default Chart;
--- pages/graphs/index.js
+++ pages/graphs/index.js
@@ -1,82 +1,30 @@
-import {useEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {getBillStatus} from "../../redux/billStatus/billStatusSlice";
-import Link from 'next/link';
 import Head from 'next/head';
-import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
+import Chart from "../../components/Chart";
+
+
+
+const chartIds = [
+  "63627b5e-8dde-455c-8549-6dfa56c43c7a",
+  "638a07a4-eca4-4b43-81bb-550f6ce019bc",
+  "638a07a4-eca4-4cc1-8b6f-550f6ce019c2",
+  "638a07a4-eca4-4329-8bf5-550f6ce019e4",
+  "638a07a4-eca4-4911-856e-550f6ce019c0",
+  "638a07a4-eca4-4f8b-83d0-550f6ce019e2",
+  "638a07a4-eca4-4f8b-83d0-550f6ce019e2",
+  "638a07a4-eca4-4e67-8651-550f6ce019c8",
+  "638a07a4-eca4-493e-841b-550f6ce019ca",
+  "638a07a4-eca4-41e5-83fd-550f6ce019c4",
+  "638a07a4-eca4-4e39-8ec9-550f6ce019de",
+  "638a07a4-eca4-451a-8456-550f6ce019cc",
+  "638a07a4-eca4-4776-8531-550f6ce019da",
+  "638a07a4-eca4-4822-89ec-550f6ce019dc",
+  "638a07a4-eca4-4344-843f-550f6ce019d0",
+  "638a07a4-eca4-4096-8522-550f6ce019e6",
+  "638a07a4-eca4-490a-877e-550f6ce019e8",
+]
+
 
 const Index = () => {
-    
-    let user = useSelector((state) => state.users)
-    const [eroId, setEroId] = useState(user?.logData?.eroList?.[0])
-
-    const sdk = new ChartsEmbedSDK({
-        baseUrl: "https://charts.mongodb.com/charts-project-0-sysep",
-    });
-    
-    // const chart = sdk.createDashboard({
-    //     dashboardId: "6188b97e-0623-4559-878a-45da62bfcbde",
-    //     height: "700px",
-    //     filter: { "spoterocd": "034" }
-    // });
-    const chart = sdk.createChart({
-      chartId: '63627b5e-8dde-455c-8549-6dfa56c43c7a',
-      height:'500px',
-      width:'100%',
-      // background:'#0f0f0f'
-      // filter: { "spoterocd": eroId }
-    });
-
-    const chart2 = sdk.createChart({
-      chartId: '638a07a4-eca4-4b43-81bb-550f6ce019bc',
-      height:'500px',
-      width:'100%',
-    });
-
-    const chart3 = sdk.createChart({
-      chartId: '638a07a4-eca4-4cc1-8b6f-550f6ce019c2',
-      height:'500px',
-      width:'100%',
-    });
-
-    const chart4 = sdk.createChart({
-      chartId: '638a07a4-eca4-4329-8bf5-550f6ce019e4',
-      height:'500px',
-      width:'100%',
-    });
-
-    // const chart5 = sdk.createChart({
-    //   chartId: '638a07a4-eca4-493e-841b-550f6ce019ca',
-    //   height:'500px',
-    //   width:'100%',
-    // });
-    
-
-    useEffect(() => {
-        chart.render(document.querySelector('.chart'))
-        chart2.render(document.querySelector('.chart2'))
-        chart3.render(document.querySelector('.chart3'))
-        chart4.render(document.querySelector('.chart4'))
-        // chart5.render(document.querySelector('.chart5'))
-        const eroIdsData = document.querySelector('.ids')
-        // const date = document.querySelector('.date')
-        eroIdsData.addEventListener('change', async (e) => {
-          if(eroIdsData.value == "") {
-            chart.setFilter({})
-          }else {
-            chart.setFilter({ "spoterocd": eroIdsData.value })
-          }
-        })
-        // date.addEventListener('change', async (e) => {
-        //   console.log(date.value)
-        //   if(eroIdsData.value == "") {
-        //     chart5.setFilter({})
-        //   }else {
-        //     chart5.setFilter({ "createdAt": new Date(date.value) })
-        //   }
-        // })
-        return () => {}
-    }, [])
 
     return (
           <div>
@@ -88,29 +36,13 @@ const Index = () => {
               <div className="card mt-3">
                 <div className="card-body">
                     <div className="row">
-                      <div className="col-lg-6">
-                          <label htmlFor="">Ero List</label>
-                          <select name="" onChange={(e) => setEroId(e.target.value)} id="" className="ids form-select mb-3">
-                            <option value={""}>{"No Select"}</option>
-                            {user?.logData?.eroList?.map((item,i) => (
-                              <option value={item} key={i}>{item}</option>
-                            ))}
-                          </select>
-                          <div className="chart"></div>
-                      </div>
-                      <div className="col-lg-6">
-                          <div className="chart2"></div>
-                      </div>
-                      <div className="col-lg-6">
-                        <div className="chart3"></div>
-                      </div>
-                      <div className="col-lg-6">
-                        <div className="chart4"></div>
-                      </div>
-                      {/* <div className="col-lg-6">
-                        <input type="date" className="date form-control" />
-                        <div className="chart5"></div>
-                      </div> */}
+                        {
+                          chartIds.map((item,index) => (
+                            <div className="col-lg-6">
+                              <Chart id={item}  name={`chart${index}`} filterId={index == 0 ? "ids" : null}  />
+                            </div>
+                          ))
+                        }
                     </div>
                 </div>
               </div>
