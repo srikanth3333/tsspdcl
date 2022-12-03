4133b50cb23509372993bdbae5e66f6587d2c989
srikanth3333 <srikanth939196@gmail.com>
2022-12-03 19:56:22 +0530

    graphs added

--- utils/getBoard.js
+++ utils/getBoard.js
@@ -3,6 +3,6 @@
 export const getBoardName = () => {
     let string = window.location.hostname
     string.substring(string.indexOf('/') + 1).split('.')[0]
-    return string;
-    // return "TSSPDCL";
+    // return string;
+    return "TSSPDCL";
 }
