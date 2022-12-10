b1a34e5821ca92e02d99da92aa4e215556f86306
srikanth3333 <srikanth939196@gmail.com>
2022-12-10 19:32:44 +0530

    output

--- utils/getBoard.js
+++ utils/getBoard.js
@@ -4,6 +4,7 @@ export const getBoardName = () => {
     let string = window.location.href
     let final = string.substring(string.indexOf('//') + 1).split('.')[0]
     let output = final.replace("/","").toUpperCase()
-    return "TSNPDCL";
-    // return output;
+    // return "TSNPDCL";
+    // return "TSSPDCL";
+    return output;
 }
